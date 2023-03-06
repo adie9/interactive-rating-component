db290672fff7ad1870e3b6033bcee767ea8c10ad
Adrian Guerra <70285960+adie9@users.noreply.github.com>
2023-03-06 14:08:05 -0500

    minor changes

--- js/main.js
+++ js/main.js
@@ -1,27 +1,21 @@
+// Initialize variables
 const ratings = document.querySelectorAll(".ratings");
+let rate_card = document.querySelector("#rate");
+let thank_you_card = document.querySelector("#thank-you");
+let submit_btn = document.querySelector("#submit-btn");
+let selection_msg = document.querySelector("#selection");
+
 let current_rating;
 
-//Create event listener fore each rating btn
+// Create event listener fore each rating btn
 ratings.forEach(ratings => {
     ratings.addEventListener("click", function() {
         document.querySelector('.active')?.classList.remove('active');
         this.classList.add('active');
         current_rating = this.innerHTML; // Get current rating selected
+        alert(current_rating);
     })
 })
 
-function submit(current_rating) {
-    let rate_card = document.querySelector("#rate");
-    let thank_you_card = document.querySelector("#thank-you");
 
-    // Check if rating is selected
-    if (current_rating == "")
-        alert("Please select a rating!")
-    else {
-        rate_card.style.display = "none";
-        thank_you_card.style.display = "block";
-    }
-}
-
-document.querySelector(".submit-btn").addEventListener("click", submit());
 
